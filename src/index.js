085f34d0b16fcd021adf0c83e577e70779cbd48f
DzianisKavalionak <nereidy4@gmail.com>
2018-10-02 10:04:15 +0300

    100

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ module.exports = function longestConsecutiveLength(array) {
         if (a < b) return -1;
     }
     array.sort(sortNumbers);
-    let result = 0;
+    if (array.length === 0) {
+        return 0;
+    }
+    let result = 1;
     for(let i = 0, len = array.length, summ = 1; i < len; i++){
         if (array[i - 1] + 1 === array[i]){
             summ++;
-            if (summ >= result) {
-                result = summ;
-            }
-        } else {
+            result = Math.max(summ, result);
+        } else
+            if(array[i - 1] !== array[i]){
             summ = 1;
-        }
+            }
     }
+
     return result;
+
+
 };
