1b50ca788ee721887a28759d0c30a3e2646e4f11
DzianisKavalionak <nereidy4@gmail.com>
2018-10-02 07:59:12 +0300

    19 fail

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    function sortNumbers(a, b) {
+        if (a > b) return 1;
+        if (a < b) return -1;
+    }
+    array.sort(sortNumbers);
+    let result = 0;
+    for(let i = 0, len = array.length, summ = 1; i < len; i++){
+        if (array[i - 1] + 1 === array[i]){
+            summ++;
+            if (summ >= result) {
+                result = summ;
+            }
+        } else {
+            summ = 1;
+        }
+    }
+    return result;
+};
