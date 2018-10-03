b5d1e2462bd4d7c8fb4c2343aa0ed4b4602be94d
DzianisKavalionak <nereidy4@gmail.com>
2018-10-03 12:41:11 +0300

    ref 100

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 module.exports = function longestConsecutiveLength(array) {
-    function sortNumbers(a, b) {
-        if (a > b) return 1;
-        if (a < b) return -1;
-    }
-    array.sort(sortNumbers);
-    if (array.length === 0) {
-        return 0;
-    }
+    array.sort((a, b) => a > b? 1 : -1)
     let result = 1;
     for(let i = 0, len = array.length, summ = 1; i < len; i++){
         if (array[i - 1] + 1 === array[i]){
             summ++;
             result = Math.max(summ, result);
         } else
-            if(array[i - 1] !== array[i]){
+        if(array[i - 1] !== array[i]){
             summ = 1;
-            }
+        }
     }
-
-    return result;
-
-
+    return array.length === 0 ? 0 : result;
 };
